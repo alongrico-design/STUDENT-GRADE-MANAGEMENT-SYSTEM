3befb7adc7f5ccaa51e0603bb1820f2ec82e093c
alongrico-design <alongrico@smcbi.edu.ph>
2026-01-28 10:59:57 +0800

    Add files via upload

---
+++ Alonglong.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Alonglong{
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("==========STUDENT GRADE MANAGEMENT SYSTEM==========");
+        System.out.println("");
+        System.out.println("[1] Add Student");
+        System.out.println("[2] View All Students");
+        System.out.println("[3] Exit");
+        System.out.println("");
+        System.out.println("Enter Choice: ");
+        int choice = sc.nextInt;
+
+        if (choice==1){
+            System.out.println("===Add Student===");
+
+
+        }else if (choice==2){
+            System.out.println("===View All Students===");
+
+
+
+        }else if (choice==3){
+            System.out.println("===Exit===");
+
+
+
+        }else {
+            System.out.println("Invalid Choice");
+
+
+        }
+
+
+
+
+
+
+
+
+
+    } 
+}
